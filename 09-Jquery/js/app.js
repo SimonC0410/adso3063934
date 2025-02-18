9605203989114328de99c69a2e3c40d4c7db8b4e
SimonC0410 <sanchezsimon0410@gmail.com>
2025-02-18 17:16:19 -0500

    Se termino las tareas de Jquery

--- 09-Jquery/js/app.js
+++ 09-Jquery/js/app.js
@@ -1,4 +1,8 @@
 $(function(){
+    //count Tasks and Remains
+    countTasks()
+    countRemains()
+    //add tasks
     $('footer').on('click', '#add', function(){
         if($('#input-task').val().length > 0){
             
@@ -9,9 +13,39 @@ $(function(){
                     </article>'
             $('section.list').append($task)
             $('#input-task').val('')
-
+            countTasks()
+            countRemains()
         } else {
             alert('Please! Enter a Task');
         }
     })
+    //Toggle Task (Remain or Done)
+    $('body').on('click', 'input[type="checkbox"]', function(){
+        //if checked
+        if ($(this).prop('checked')){
+            $(this).parent().addClass('checked')
+        }else{
+            $(this).parent().removeClass('checked')
+        }
+        countRemains()
+    })
+    //Remove Task
+    $('body').on('click', 'article button', function() {
+        $(this).closest('article').remove()
+        countTasks()
+        countRemains()
+    })
 })
+// Count Tasks
+function countTasks(){
+    $('.num-tasks').text($('article').length)
+    $('.title-tasks').text($('article').length > 1 ? 'Tasks' : 'Task')
+}
+
+//count Remains
+
+function countRemains(){
+    $remain = Math.abs($('.checked').length - $('article').length)
+    $('.num-remains').text($remain)
+    $('.title-remains').text($remain >1 ? 'Remains' : 'Remain')
+}
